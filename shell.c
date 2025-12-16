d7650a1c3db3fe1e71c187466c2076888c6f880c
MattW-12106 <12106@holbertonstudents.com>
2025-12-16 23:43:18 +0000

    added environ + header file + now frees argv + now handles Ctrl+D + create argv function now allocates correct memory space and removes newline

--- shell.c
+++ shell.c
@@ -1,8 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
+#include "shell.h"
+
+extern char **environ;
 
 /**
  * create_argv - create argv to pass to execve
@@ -13,40 +11,25 @@
  */
 char **create_argv(char *input)
 {
-	char *s = input;
 	char *token;
-	const char delim[2] = " ";
 	char **argv;
-	unsigned int len = 0;
-	int i = 0;
 
-	while (*s != '\0')
-	{
-		if (*s == ' ')
-		{
-			len++;
-		}
-		s++;
-	}
-
-	argv = malloc(sizeof(char *) * len);
+	argv = malloc(sizeof(char *) * 2);
 	if (argv == NULL)
+		return (NULL);
+
+	token = strtok(input, "\n");
+	if (token == NULL)
 	{
+		free(argv);
 		return (NULL);
 	}
-	token = strtok(input, delim);
-	while (token != NULL)
-	{
-		/*
-		 * Edit the token to remove all non characters (i.e \n '\0')
-		 */
-		argv[i++] = token;
-		printf("%ld |%ld\n ", strlen(token), sizeof(token));
-		token = strtok(NULL, delim);
 
-	}
+	argv[0] = token
+	argv[1] = NULL;
 	return argv;
 }
+
 /**
  * main - the main function, starts the shell process
  *
@@ -62,43 +45,42 @@ int main (void)
 
 	pid_t new_process;
 
-	while (line != -1)
+	while (1)
 	{
 		printf("[H_Shell] $ ");
-		/* 
-		 * Getline is blocking, so it won't continue
-		 * until \n or EOF is written to stdin.
-		 */
+
 		line = getline(&input_line, &input_len, stdin);
-		printf("%s\n", input_line);
+		if (line == -1)
+		{
+			printf("\n");
+			break;
+		}
 
 		argv = create_argv(input_line);
 		if (argv == NULL)
 		{
-			perror("ERROR: ");
-			return (1);
+			continue;
 		}
 
 		new_process = fork();
 		if (new_process == -1)
 		{
 			perror("ERROR: ");
-			return (1);
+			free(argv);
+			continue;
 		}
 		if (new_process == 0)
 		{
-			if (execve(argv[0], argv, NULL) == -1)
-			{
-				perror("ERROR: ");
-				return (1);
-			}
+			execve(argv[0], argv, environ);
+			perror("ERROR: ");
+			exit(1);
 		}
 		else
 		{
 			wait(&status);
+			free(argv);
 		}
 	}
-	/* free argv[] */
 	free(input_line);
 	return (0);
 }
---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* Libraries (add to when needed) */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/* We can also put function in here later if needed too */
+
+
+
+#endif /* SHELL_H */
