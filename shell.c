5be78cb4251b2b44830cf1f6a43d1b7c90de3a80
MaxB-Holberton <12107@holbertonstudents.com>
2026-01-09 09:58:19 +1100

    resolved surface issues, added env and exit handling, moving to check valgrind and then run against the checkers

--- shell.c
+++ shell.c
@@ -6,7 +6,7 @@ extern char **environ;
  * new_fork - checks PATH and create a new process
  * @argv: the list of commands to execute
  *
- * Return: 0 - parent return | 1 - execve fails
+ * Return: 0 - parent return | -1 - execve fails
  */
 int new_fork(char **argv)
 {
@@ -16,43 +16,34 @@ int new_fork(char **argv)
 	new_process = fork();
 	if (new_process == -1)
 	{
-		return (1);
+		return (-1);
 	}
 	if (new_process == 0)
 	{
 		execve(argv[0], argv, environ);
 		perror("ERROR - failed child");
-		return (1);
+		return (-1);
 		/* should only return on failed call */
 	}
-	else
-	{
-		wait(&status);
-		free(argv);
-		return (0);
-	}
-	return (1);
+	waitpid(new_process, &status, 0);
+	return (0);
 }
 
 /**
  * check_path - checks the Path for the requested exe file
  * @argv: the list of arguments
- * @env_list: the PATH env as a list
+ * @path_list: the PATH env as a list
  *
- * Return: 0 if new_fork returns from parent | 1 if new_fork child fails
+ * Return: 0 success | -1 if new_fork child fails | 1 on generic fail
  */
-int check_path(char **argv, char **env_list)
+int check_path(char **argv, char **path_list)
 {
 	size_t i = 0;
 	size_t len = 0;
 	size_t argv_0_len = 0;
 	char *full_path;
-	char *argv_0_dup;
 	int rtn = 0;
 
-	/*
-	 * TODO: add 'exit' & 'env' handling here
-	 */
 	if (strchr(argv[0], '/') != NULL)
 	{
 		/* checking if the command already contains a path */
@@ -60,20 +51,20 @@ int check_path(char **argv, char **env_list)
 		{
 			return(new_fork(argv));
 		}
-		printf("ERROR: %s not found\n", argv[0]);
-		return (0);
+		return (1);
 	}
-	argv_0_len = strlen(argv[0]) + 1;
-	/* searching in PATH */
-	for (i = 0; env_list[i] != NULL; i++)
+
+	argv_0_len = strlen(argv[0]) + 2;
+	for (i = 0; path_list[i] != NULL; i++)
 	{
-		len = strlen(env_list[i]) + argv_0_len;
+		/* malloc each path_list item w/ argv_0 */
+		len = strlen(path_list[i]) + argv_0_len;
 		full_path = malloc(len);
 		if (!full_path)
 			return (1);
 
-		strcpy(full_path, env_list[i]);
-		strcat(full_path, "/");
+		strcpy(full_path, path_list[i]);
+		strcat(full_path, "/\0");
 		strcat(full_path, argv[0]);
 		if (access(full_path, X_OK) == 0)
 		{
@@ -83,11 +74,39 @@ int check_path(char **argv, char **env_list)
 			full_path = NULL;
 			return(rtn);
 		}
+		free(full_path);
 	}
-	free(full_path);
 	full_path = NULL;
-	printf("ERROR: %s not found\n", argv[0]);
-	return (0);
+	return (1);
+}
+/**
+ * pre_process - handles reserved argv_0 lines
+ * @argv: the arguments
+ * @path_list: the list of env PATHs
+ *
+ * Return: 0 on sucess
+ */
+int pre_process(char **argv, char** path_list)
+{
+	unsigned int i = 0;
+	if (argv[0] == NULL)
+	{
+		return(0);
+	}
+	if (strcmp(argv[0], "exit") == 0)
+	{
+		return (2);
+	}
+	if (strcmp(argv[0], "env") == 0)
+	{
+		while (environ[i] != NULL)
+		{
+			printf("%s\n", environ[i]);
+			i++;
+		}
+		return (0);
+	}
+	return (check_path(argv, path_list));
 }
 
 /**
@@ -101,10 +120,10 @@ int main(void)
 	size_t input_len = 0;
 	ssize_t line_len;
 	char **argv;
-	char **env_list;
-	int process_status = 0;
+	char **path_list;
+	int status = 0;
 
-	env_list = create_env_list("PATH", environ);
+	path_list = create_env_list("PATH", environ);
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
@@ -115,7 +134,9 @@ int main(void)
 		if (line_len == -1)
 		{
 			/* EoF reached */
-			printf("\n");
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+
 			break;
 		}
 		/* trim the string and then create argv */
@@ -124,22 +145,25 @@ int main(void)
 		{
 			continue;
 		}
-		if (argv[0] == NULL)
+		status = pre_process(argv, path_list);
+		if (status == -1 || status == 2)
 		{
-			continue;
-		}
-		process_status = check_path(argv, env_list);
-		if (process_status == 1)
-		{
-			/* If child fails to execute */
-			perror("ERROR");
+			/* if 'exit' or child failed to execve */
 			free(argv);
-			free(input_line);
-			free(env_list);
-			return (1);
+			break;
 		}
+		if (status == 1)
+		{
+			printf("ERROR: %s not found\n", argv[0]);
+		}
+		free(argv);
 	}
-	free(env_list);
+	free(path_list);
 	free(input_line);
+	if (status == -1)
+	{
+		perror("ERROR");
+		return (1);
+	}
 	return (0);
 }
